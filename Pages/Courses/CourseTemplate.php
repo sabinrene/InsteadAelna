107438e2e48a7de9349539885bd944d2fe867c11
Tidus <Tidus@hotmail.com>
2020-08-06 17:12:57 -0500

    ...

--- Pages/Courses/CourseTemplate.php
+++ Pages/Courses/CourseTemplate.php
@@ -263,66 +263,8 @@
 			</a></div>
 
 
-
 		</div>
     <!-- END CourseTemp Right -->
-
-        <div id="timeCourse" class="Row2 clearfix">
-
-            <div class="Schedule">
-              <h3> Course Schedule </h3>
-                <table id="Schedule" class="Schedule">
-
-                <!--  <tr>
-                    <th> Mo </th>
-                    <td> 10am - 1pm </td>
-                    <td>  </td>
-                  </tr>
-
-                  <tr>
-                    <th> Tu </th>
-                    <td class="OddRow"> 2pm - 4pm </td>
-                    <td>  </td>
-                  </tr>
-
-                  <tr>
-                    <th> Wd </th>
-                    <td> 12pm - 3pm </td>
-                    <td>  </td>
-                  </tr>
-
-                  <tr>
-                    <th> Th </th>
-                    <td class="OddRow"> 2pm - 4pm </td>
-                    <td>  </td>
-                  </tr>
-
-                  <tr>
-                    <th> F </th>
-                    <td> 12pm - 3pm </td>
-                    <td>  </td>
-                  </tr>
-
-                  <tr>
-                    <th> Sat </th>
-                    <td> 12pm - 3pm </td>
-                    <td>  </td>
-                  </tr>
-
-                  <tr>
-                    <th> Sun </th>
-                    <td> 12pm - 3pm </td>
-                    <td>  </td>
-                  </tr>-->
-
-                </table>
-
-            </div>
-
-    </div>
-    <!-- END Row2 -->
-
-
 
 	</div>
 	<!-- End RIGHT -->
